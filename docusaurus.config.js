66abd6f859984f26915d2c85ec81b03b376aec21
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-10-03 14:12:18 +0300

    update configs for Github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,9 +24,12 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'technix', // Usually your GitHub org/user name.
   projectName: 'atrament.ink', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  trailingSlash: true,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
